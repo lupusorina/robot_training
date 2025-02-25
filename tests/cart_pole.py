efa9a6cb97a843e748aa76f27b69b5faa6b4cbeb
Sorina <lupusorina@yahoo.com>
2025-02-25 10:27:05 -0800

    move xml in the cartpole and add obs and action space size

--- tests/cart_pole.py
+++ tests/cart_pole.py
@@ -4,14 +4,46 @@ import jax.numpy as jnp
 from brax.io import mjcf, html
 import numpy as np
 
+
+XML_MODEL = """
+<mujoco model="inverted pendulum">
+    <compiler inertiafromgeom="true"/>
+
+    <default>
+        <joint armature="0" damping="1"/>
+        <geom contype="0" conaffinity="0" friction="1 0.1 0.1"/>
+    </default>
+
+    <worldbody>
+        <light diffuse=".5 .5 .5" pos="0 0 3" dir="0 0 -1"/>
+        <geom name="rail" type="capsule"  size="0.02 1.5" pos="0 0 0" quat="1 0 1 0" rgba="1 1 1 1"/>
+        <body name="cart" pos="0 0 0">
+            <joint name="slider" type="slide" axis="1 0 0" pos="0 0 0" limited="true" range="-1.5 1.5"/>
+            <geom name="cart_geom" pos="0 0 0" quat="1 0 1 0" size="0.1 0.1" type="capsule"/>
+            <body name="pole" pos="0 0 0">
+                <joint name="hinge" type="hinge" axis="0 1 0" pos="0 0 0"/>
+                <geom name="pole_geom" type="capsule" size="0.049 0.3"  fromto="0 0 0 0.001 0 0.6"/>
+            </body>
+        </body>
+    </worldbody>
+
+    <actuator>
+        <motor ctrllimited="true" ctrlrange="-3 3" gear="100" joint="slider" name="slide"/>
+    </actuator>
+
+</mujoco>
+"""
+
 class CartPoleJax(PipelineEnv):
     """ Environment for training Cart Pole balancing """
 
-    def __init__(self, xml_model: str, backend: str = 'mjx', **kwargs):
+    def __init__(self, backend: str = 'mjx', **kwargs):
         # Initialize System:
-        sys = mjcf.loads(xml_model)
+        sys = mjcf.loads(XML_MODEL)
         self.step_dt = 0.02
         n_frames = kwargs.pop('n_frames', int(self.step_dt / sys.opt.timestep))
+        self.observation_space = 4
+        self.action_space = 1
         super().__init__(sys, backend=backend, n_frames=n_frames)
 
     def reset(self, rng: jax.Array) -> State:
