ce8c0f5a462c42ea525724f3d286a4a7f02cc0b9
Sorina <lupusorina@yahoo.com>
2025-02-26 14:46:08 -0800

    remove testing

--- src/train.py
+++ src/train.py
@@ -8,6 +8,21 @@ if subprocess.run('nvidia-smi').returncode:
       'Go to the Runtime menu and select Choose runtime type.'
   )
 
+# Add an ICD config so that glvnd can pick up the Nvidia EGL driver.
+# This is usually installed as part of an Nvidia driver package, but the Colab
+# kernel doesn't install its driver via APT, and as a result the ICD is missing.
+# (https://github.com/NVIDIA/libglvnd/blob/master/src/EGL/icd_enumeration.md)
+NVIDIA_ICD_CONFIG_PATH = '/usr/share/glvnd/egl_vendor.d/10_nvidia.json'
+if not os.path.exists(NVIDIA_ICD_CONFIG_PATH):
+  with open(NVIDIA_ICD_CONFIG_PATH, 'w') as f:
+    f.write("""{
+    "file_format_version" : "1.0.0",
+    "ICD" : {
+        "library_path" : "libEGL_nvidia.so.0"
+    }
+}
+""")
+
 xla_flags = os.environ.get('XLA_FLAGS', '')
 xla_flags += ' --xla_gpu_triton_gemm_any=True'
 os.environ['XLA_FLAGS'] = xla_flags
@@ -129,80 +144,4 @@ FOLDER_SAVE = 'logs'
 if not os.path.exists(FOLDER_SAVE):
     os.makedirs(FOLDER_SAVE)
 model.save_params(f'{FOLDER_SAVE}/{MODEL_NAME}', params)
-
 print("Training Complete")
-
-eval_env = env
-jit_reset = jax.jit(eval_env.reset)
-jit_step = jax.jit(eval_env.step)
-jit_inference_fn = jax.jit(make_inference_fn(params, deterministic=True))
-print(f'JITing reset and step')
-reset_fn = jax.jit(env.reset)
-step_fn = jax.jit(env.step)
-
-rng = jax.random.PRNGKey(1)
-
-rollout = []
-modify_scene_fns = []
-
-x_vel = 1.0  #@param {type: "number"}
-y_vel = 0.0  #@param {type: "number"}
-yaw_vel = 0.0  #@param {type: "number"}
-command = jp.array([x_vel, y_vel, yaw_vel])
-
-phase_dt = 2 * jp.pi * eval_env.ctrl_dt * 1.5
-phase = jp.array([0, jp.pi])
-
-for j in range(1):
-  print(f"episode {j}")
-  state = jit_reset(rng)
-  state.info["phase_dt"] = phase_dt
-  state.info["phase"] = phase
-  for i in range(1000):
-    act_rng, rng = jax.random.split(rng)
-    ctrl, _ = jit_inference_fn(state.obs, act_rng)
-    state = jit_step(state, ctrl)
-    if state.done:
-      break
-    state.info["command"] = command
-    rollout.append(state)
-
-    xyz = np.array(state.data.xpos[eval_env._mj_model.body("torso").id])
-    xyz += np.array([0, 0.0, 0])
-    x_axis = state.data.xmat[eval_env._torso_body_id, 0]
-    yaw = -np.arctan2(x_axis[1], x_axis[0])
-    modify_scene_fns.append(
-        functools.partial(
-            draw_joystick_command,
-            cmd=state.info["command"],
-            xyz=xyz,
-            theta=yaw,
-            scl=np.linalg.norm(state.info["command"]),
-        )
-    )
-
-render_every = 1
-fps = 1.0 / eval_env.ctrl_dt / render_every
-print(f"fps: {fps}")
-traj = rollout[::render_every]
-mod_fns = modify_scene_fns[::render_every]
-
-scene_option = mujoco.MjvOption()
-scene_option.geomgroup[2] = True
-scene_option.geomgroup[3] = False
-scene_option.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
-scene_option.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = False
-scene_option.flags[mujoco.mjtVisFlag.mjVIS_PERTFORCE] = False
-
-frames = eval_env.render(
-    traj,
-    camera="track",
-    scene_option=scene_option,
-    width=640*2,
-    height=480,
-    modify_scene_fns=mod_fns,
-)
-
-media.show_video(frames, fps=fps, loop=False)
-
-media.write_video(f'{VIDEO_FOLDER}/joystick.mp4', frames, fps=fps)
