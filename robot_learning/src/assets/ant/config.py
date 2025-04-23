f01e53f746b4c729570f21fd0fc477e34ee92eef
Sorina <lupusorina@yahoo.com>
2025-04-23 10:48:04 -0700

    name for the joints of the ant and add torso id

--- robot_learning/src/assets/ant/config.py
+++ robot_learning/src/assets/ant/config.py
@@ -1,4 +1,5 @@
 import os
 
 parent_dir = os.path.abspath(os.path.join(os.getcwd()))
-XML_PATH = os.path.join(parent_dir, '../assets/ant/xmls/ant.xml')
+XML_PATH = os.path.join(parent_dir, '../../assets/ant/xmls/ant.xml')
+ROOT_BODY = "torso"
