aea36fa25c05d4db0b559dfe5c9da4a104bdf705
Sorina <lupusorina@yahoo.com>
2025-04-25 21:41:27 -0700

    cleanup previous xml

--- robot_learning/src/assets/pendulum/config.py
+++ robot_learning/src/assets/pendulum/config.py
@@ -2,4 +2,4 @@ import os
 
 parent_dir = os.path.abspath(os.path.join(os.getcwd()))
 XML_PATH = os.path.join(parent_dir, '../assets/pendulum/xmls/pendulum.xml')
-ROOT_BODY = "pole"
+ROOT_BODY = "pendulum_base"
