f4042b0fea73c504e0156c33c99056a7b44fce9f
Sorina <lupusorina@yahoo.com>
2025-06-29 17:23:01 -0700

    use v2 of the biped

--- robot_learning/src/jax/envs/biped.py
+++ robot_learning/src/jax/envs/biped.py
@@ -29,7 +29,7 @@ import time
 # Constants.
 NAME_ROBOT = 'biped'
 if NAME_ROBOT == 'biped':
-  import robot_learning.src.assets.biped_no_yaw.config as robot_config
+  import robot_learning.src.assets.biped_no_yaw_v2.config as robot_config
 else:
   raise ValueError(f'NAME_ROBOT must be "biped"')
 print('NAME_ROBOT:', NAME_ROBOT)
