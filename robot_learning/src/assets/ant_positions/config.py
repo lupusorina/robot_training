e7d1fd231e20b6a2f63a73ccfe5eaf49937f01ce
Sorina <lupusorina@yahoo.com>
2025-07-18 10:04:20 -0700

    ant position

---
+++ robot_learning/src/assets/ant_positions/config.py
@@ -0,0 +1,6 @@
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd()))
+XML_PATH = os.path.join(parent_dir, '../assets/ant_positions/xmls/ant.xml')
+ROOT_BODY = "torso"
+GRAVITY_SENSOR = "upvector"
